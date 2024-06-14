bac9630f56af19cc160af480ad4f8dd960e8c638
Michael Stark <michaelstark@MacBook-Air-von-Michael-2.local>
2024-06-14 13:36:14 +0200

    new js 3

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ function preload(){
 
 
 function setup() {
-  let canvas = createCanvas(360, 800);
+  let canvas = createCanvas(windoWidth, windowHeight*0.8);
   canvas.id('canvas-container');
   canvas.style('display', 'block');
   canvas.style('margin', 'auto');
