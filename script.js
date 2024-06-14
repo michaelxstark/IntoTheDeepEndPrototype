dfab8ca36ae2b052a1212504e679fe1555fa12d7
Michael Stark <michaelstark@MacBook-Air-von-Michael-2.local>
2024-06-14 13:01:12 +0200

    new files

--- script.js
+++ script.js
@@ -393,3 +393,24 @@ function mousePressed(){
   }
 
 }
+
+
+
+function touchStarted () {
+  var fs = fullscreen();
+  if (!fs) {
+    fullscreen(true);
+  }
+}
+
+/* full screening will change the size of the canvas */
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+/* prevents the mobile browser from processing some default
+ * touch events, like swiping left for "back" or scrolling the page.
+ */
+document.ontouchmove = function(event) {
+    event.preventDefault();
+};
